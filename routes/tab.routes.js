6ec54ef22c5de2779ef1671ec9998c56a65aa5a9
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-12-05 01:29:02 -0300

    link financeiro

--- routes/tab.routes.js
+++ routes/tab.routes.js
@@ -7,8 +7,23 @@ import Avisos from '../Components/Avisos';
 import Profile from '../Components/Profile';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useAuth } from '../AuthContext';  // Certifique-se de que o caminho esteja correto
+import { Linking, TouchableOpacity, View, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
+
+function ExternalLink() {
+  const handlePress = () => {
+    Linking.openURL('https://www.sp.senac.br/fale-com-a-gente');
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={handlePress} style={{ padding: 10, backgroundColor: '#D93083', borderRadius: 5 }}>
+        <Text style={{ color: '#fff', fontSize: 16 }}>Acessar Fale com a Gente</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 export default function TabRoutes() {
   const { user } = useAuth();  // Aqui você pode acessar o usuário
@@ -28,6 +43,8 @@ export default function TabRoutes() {
             iconName = 'clock-o';
           } else if (route.name === 'Avisos') {
             iconName = 'bell';
+          } else if (route.name === 'Fale com a Gente') {
+            iconName = 'envelope';
           }
 
           return <Icon name={iconName} size={size} color={color} />;
@@ -36,10 +53,10 @@ export default function TabRoutes() {
         tabBarInactiveTintColor: '#656CEE',
       })}
     >
-      
       <Tab.Screen name="Home" component={Home} initialParams={{ userData: user }} />
       <Tab.Screen name="Conceitos" component={Matter} />
       <Tab.Screen name="Avisos" component={Avisos} />
+      <Tab.Screen name="Fale com a Gente" component={ExternalLink} />
     </Tab.Navigator>
   );
 }
